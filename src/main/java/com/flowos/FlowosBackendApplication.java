d6bf59e6d4a1c47d0c5f8e5d1346969b65ff11f0
namramoradiya <moradiyanamra@gmail.com>
2026-08-08 12:44:37 +0530

    Initial FlowOS backend

---
+++ src/main/java/com/flowos/FlowosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flowos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowos/FlowosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
